c64bef6673c02ecb4b4af504a93564074a4429e0
Jesús Manuel García Muñoz <jesus@deliriumcoder.com>
2017-01-28 03:56:29 +0100

    Added engine.rb.

--- lib/mybookings_adobe_connect.rb
+++ lib/mybookings_adobe_connect.rb
@@ -1,3 +1,5 @@
+require 'mybookings_adobe_connect/engine'
+
 require 'mybookings/resource_types_extensions/adobe_connect_extension'
 
 module MybookingsAdobeConnect
---
+++ lib/mybookings_adobe_connect/engine.rb
@@ -0,0 +1,5 @@
+module MybookingsAdobeConnect
+  class Engine < ::Rails::Engine
+    isolate_namespace MybookingsAdobeConnect
+  end
+end
