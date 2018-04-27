76dc6d588bbef7379701cefe9191dc96070a6839
Jose Manuel Martinez Gallardo <jose@deliriumcoder.com>
2018-04-27 13:21:41 +0200

    Fix an error when creating a public adobe meeting room.

--- app/controllers/mybookings/adobe_connect_bookings_controller.rb
+++ app/controllers/mybookings/adobe_connect_bookings_controller.rb
@@ -65,7 +65,7 @@ module Mybookings
     end
 
     def preprocess_adobe_connect_participants
-      emails_list = params[booking_type.model_name.param_key][:adobe_connect_participants]
+      emails_list = params[booking_type.model_name.param_key][:adobe_connect_participants] || ''
       emails_list = emails_list.split(',').map { |e| e.gsub(/\s+/, '') }
       params[booking_type.model_name.param_key][:adobe_connect_participants] = emails_list
     end
